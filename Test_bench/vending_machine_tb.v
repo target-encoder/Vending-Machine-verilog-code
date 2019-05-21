761e472550164a7ac2642aeaa140d504d38f5bc3
target-encoder <44730674+target-encoder@users.noreply.github.com>
2019-05-22 01:17:47 +0300

    Vending Machine Verilog code
    Verilog code Vending Machine

---
+++ Test_bench/vending_machine_tb.v
@@ -0,0 +1,265 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   19:55:25 01/01/2019
+// Design Name:   source_code
+// Module Name:   D:/Fiver_deliveries/Vending_machine_cola_water/vending_machine_tb.v
+// Project Name:  Vending_machine_cola_water
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: source_code
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module vending_machine_tb;
+
+	// Inputs
+	reg selection;
+	reg clk;
+	reg [1:0] coin;
+	reg rst;
+	reg dispense;
+
+	// Outputs
+	wire cola;
+	wire water;
+	wire [9:0] change;
+	wire [9:0] balance;
+
+	// Instantiate the Unit Under Test (UUT)
+	source_code uut (
+		.selection(selection), 
+		.cola(cola), 
+		.water(water), 
+		.clk(clk), 
+		.coin(coin), 
+		.rst(rst), 
+		.change(change), 
+		.dispense(dispense), 
+		.balance(balance)
+	);
+
+
+ always 
+   #10  clk = ! clk; 
+
+	initial begin
+		// Initialize Inputs
+		selection = 0;
+		clk = 00;
+		coin = 0;
+		rst = 1;
+		dispense = 0;
+
+		// Wait 100 ns for global reset to finish
+		//comb1  water with no change
+		#100;
+		rst=0;
+		coin = 2'b10;// 50 krs
+		selection = 0;
+		dispense = 0;
+		
+		#25 
+		coin = 2'b01;// 1Tl
+		selection = 0;
+		dispense = 0;
+		
+		#25
+		coin = 2'b10;// 50krs
+		selection = 0; //water
+		dispense = 1;
+		
+		//comb2 water with change
+		#100
+		rst=1;
+		#25
+		rst=0;
+		#50
+		coin = 2'b10;// 1 Tl
+		selection = 0;
+		dispense = 0;
+		
+		#50
+		coin = 2'b01;// 50 krs
+		selection = 0;
+		dispense = 0;
+		
+		#50
+		coin = 2'b10;// 1 Tl
+		selection = 0;
+		dispense = 0;
+		
+		//comp3 water with 150 krs change
+		#100
+		rst=1;
+		#25
+		rst=0;
+		#25
+		coin = 2'b10;// 1 Tl
+		selection = 0;
+		dispense = 0;
+		
+		#25
+		coin = 2'b01;// 50 krs
+		selection = 0;
+		dispense = 0;
+		
+		#25
+		coin = 2'b10;// 1 Tl
+		selection = 0;
+		dispense = 0;
+		#25
+		coin = 2'b01;// 50krs 
+		selection = 0;
+		dispense = 0;
+		#25
+		dispense=1;
+		//comp4 no balance for water 
+		#100
+		rst=1;
+		#25
+		rst=0;
+		#25
+		coin = 2'b10;// 1 Tl
+		selection = 0;
+		dispense = 0;
+		
+		#25
+		coin = 2'b00;
+		selection = 0;
+		dispense = 0;
+		#25
+		dispense=1;
+		
+		//comb5 cola with no change
+		#100;
+		rst=0;
+		coin = 2'b10;// 50 krs
+		selection = 1;
+		dispense = 0;
+		
+		#25 
+		coin = 2'b01;// 1Tl
+		selection = 1;
+		dispense = 0;
+		
+		#25 
+		coin = 2'b01;// 50 krs
+		selection = 1;
+		dispense = 0;
+		
+		#25
+		coin = 2'b10;// 50krs
+		selection = 1; //cola
+		dispense = 1;
+		
+		//comb6 cola with change 50
+		#100
+		rst=1;
+		#25
+		rst=0;
+		#50
+		coin = 2'b10;// 1 Tl
+		selection = 1;
+		dispense = 0;
+		
+		#50
+		coin = 2'b01;// 50 krs
+		selection = 1;
+		dispense = 0;
+		
+		#50
+		coin = 2'b10;// 1 Tl
+		selection = 1;
+		dispense = 0;
+		
+		//comp7 cola with 100 krs change
+		#100
+		rst=1;
+		#25
+		rst=0;
+		#25
+		coin = 2'b10;// 1 Tl
+		selection = 1;
+		dispense = 0;
+		
+		#25
+		coin = 2'b01;// 50 krs
+		selection = 1;
+		dispense = 0;
+		
+		#25
+		coin = 2'b10;// 1 Tl
+		selection = 1;
+		dispense = 0;
+		#25
+		coin = 2'b01;// 50krs 
+		selection = 1;
+		dispense = 0;
+		#25
+		coin = 2'b10;// 1 Tl 
+		selection = 1;
+		dispense = 0;
+		
+		#25
+		dispense=1;
+		//comp8 no balance for cola 
+		#100
+		rst=1;
+		#25
+		rst=0;
+		#25
+		coin = 2'b00;// 1 Tl
+		selection = 1;
+		dispense = 0;
+		
+		#25
+		coin = 2'b00;// 50 krs
+		selection = 1;
+		dispense = 0;
+		
+		#25 //no input coins
+		coin = 2'b00;
+		selection = 1;
+		dispense = 0;
+		#25
+		dispense=1;
+		//comp9 only one TL with no dispense order
+		#100
+		rst=1;
+		#25
+		rst=0;
+		#25
+		coin = 2'b10;// 1 Tl
+		selection = 1;
+		dispense = 0;
+		
+		//comp10 dispense_order with no balance
+		#100
+		rst=1;
+		#25
+		rst=0;
+		#25
+		coin = 2'b01;// 50krs 
+		selection = 1;
+		dispense = 0;
+	
+        
+        
+		  
+
+	end
+      
+endmodule
+
---
+++ Verilog_source_code/source_code.v
@@ -0,0 +1,127 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:50:58 01/01/2019 
+// Design Name: 
+// Module Name:    source_code 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module source_code(selection, cola,water,clk,coin,rst, change,dispense, balance);
+
+   input wire        selection; // 0 for water and 1 for cola
+	input wire [1:0]  coin;
+   input wire        clk,rst;
+	input wire        dispense; //dispense button
+	
+   output reg cola;
+	output reg water;
+   output reg [9:0] change;
+
+	
+   
+   reg [1:0]   state;
+   reg [1:0]   next_state;
+	output reg [9:0]  balance; // max balance in one process is (2^(10-1)) ~ 500 kurus= 5 TL
+	
+	parameter s0=2'b00;
+	parameter s1=2'b01;
+	parameter s2=2'b10;
+	
+
+
+   
+  
+	
+	
+  initial begin
+  balance =10'b0;
+  change =0;
+  water <=0;
+  cola <=0;
+ 
+  end
+
+   always @(posedge clk)
+     begin
+       if (rst)
+		 
+					state<=s2;
+					
+		
+       else
+         state<=next_state;
+     end
+
+
+   always @(state,coin,dispense,selection,balance)
+     begin
+		 case (state)
+		  s0:
+			begin 
+				  if (dispense==1'b0)
+				  begin
+					  if (coin==2'b00 || coin==2'b11); //no coin
+					  else if (coin==2'b01) // 50 krs
+					  balance=balance+10'd50;
+					  else if (coin==2'b10)// 1 Tl
+					  balance=balance+10'd100;
+					  
+					  next_state=s0;
+					end
+				else if (dispense==1'b1) 
+				  next_state= s1;
+			end  
+		 s1:
+		 begin
+			 if (selection==1'b0)
+				 begin //water
+					  if (balance >=150) //1.5TL
+					  begin
+						  water<=1'b1;
+						  cola<=1'b0;
+						  change=balance-10'd150;
+						end
+						
+					 next_state=s2;
+				  end  
+				  
+			  else if (selection==1'b1)
+				  begin //cola
+					  if (balance >=250) //2.5TL
+					  begin
+						  cola<=1'b1;
+						  water<=1'b0;
+						  change=balance-10'd250;
+					  end
+					next_state=s2;
+			    end
+			end
+		 
+		s2 :
+		 begin
+				 balance =10'b0;
+				 change =10'b0;
+				  water <=0;
+				  cola <=0;
+				next_state=s0; 
+		 end
+		 
+				
+	default : next_state=s2;
+	
+  endcase // case (state)
+ end // always @ (state,next_state)
+	  
+endmodule
